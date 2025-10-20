ff3fde1836a0012f3d88c8ce1dee77b05dce1142
dltmdrb667 <dltmdrb667@naver.com>
2025-10-20 11:48:35 +0900

    메모이제이션 실습예제

--- workspace/js/mylib.js
+++ workspace/js/mylib.js
@@ -19,3 +19,26 @@ export const mylib = {};
 export const test = () => {
   console.log("안녕하세요");
 };
+
+
+// isPrime(5) 메모이제이션 안됨
+// isPrime = isPrime.memoize();
+// isPrime(5) 메모이제이션이 됨
+Function.prototype.memoize = function(){
+  // this = isPrime
+  const fn = this;
+  return function(){
+    return fn.memo.apply(fn, arguments); // isPrime.memo(5)
+  };
+};
+
+// 모든 함수에 메모이제이션 기능 추가
+Function.prototype.memo = function(key){
+  // 캐시를 위한 코드
+  this._cache = this._cache || {};
+  if(this._cache[key] !== undefined){
+    return this._cache[key];
+  }else{
+    return this._cache[key] = this(key);
+  }
+};
