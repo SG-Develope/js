f783f8da610b34372727ba7eaccef3cfd7d93eec
dltmdrb667 <dltmdrb667@naver.com>
2025-10-28 16:18:29 +0900

    Enum타입 실습

--- workspace/ch06/ex06-02-02.js
+++ workspace/ch06/ex06-02-02.js
@@ -1,12 +1,59 @@
-"use strict";
 // Enum 타입
-Object.defineProperty(exports, "__esModule", { value: true });
 (() => {
-    let str = 'hello';
-    let age = 5;
-    let done = true;
-    // done = 'false'; // 컴파일 에러
-    let nullVal = null;
-    let emptyVal;
-    let todo = { title: 'TypeScript 공부', done: false };
+    function printLanguage(lang) {
+        switch (lang) {
+            case 'JavaScript':
+                console.log('자바스크립트 개발자군요!');
+                break;
+            case 'Python':
+                console.log('파이썬 개발자군요!');
+                break;
+            case 'C':
+                console.log('C언어 개발자군요!');
+                break;
+            case 'Java':
+                console.log('자바 개발자군요!');
+                break;
+        }
+    }
+    printLanguage('Java');
+    printLanguage('C');
+    printLanguage('C++');
+    printLanguage('Python');
+    printLanguage('Javascript');
 })();
+(() => {
+    let Language;
+    (function (Language) {
+        Language["JavaScript"] = "JS";
+        Language["C"] = "C";
+        Language["Python"] = "PY";
+        Language["Java"] = "J"; // 3
+    })(Language || (Language = {}));
+    console.log(Language.Python, Language.C);
+    function printLanguage(lang) {
+        switch (lang) {
+            case Language.JavaScript:
+                console.log('자바스크립트 개발자군요!');
+                break;
+            case Language.Python:
+                console.log('파이썬 개발자군요!');
+                break;
+            case Language.C:
+                console.log('C언어 개발자군요!');
+                break;
+            case Language.Java:
+                console.log('자바 개발자군요!');
+                break;
+        }
+    }
+    // printLanguage(Language.JavaScript);
+    // printLanguage(Language.C);
+    // printLanguage(Language.Java);
+    // printLanguage(Language.Python);
+    // printLanguage(Language.JavaScript);
+    // printLanguage(Language.C);
+    // printLanguage(Language.Java);
+    // printLanguage(Language.Python);
+})();
+export {};
--- workspace/ch06/ex06-02-02.ts
+++ workspace/ch06/ex06-02-02.ts
@@ -1,14 +1,64 @@
 // Enum 타입
 
 (()=>{
-  let str: string = 'hello';
-  let age: number = 5;
-  let done : boolean = true;
-  // done = 'false'; // 컴파일 에러
-  
-  let nullVal: null= null;
-  let emptyVal : undefined;
+  function printLanguage(lang: string){
+    switch(lang){
+      case 'JavaScript':
+        console.log('자바스크립트 개발자군요!');
+        break;
+      case 'Python':
+        console.log('파이썬 개발자군요!');
+        break;
+      case 'C':
+        console.log('C언어 개발자군요!');
+        break;
+      case 'Java':
+        console.log('자바 개발자군요!');
+        break;
+    }
+  }
+  printLanguage('Java');
+  printLanguage('C');
+  printLanguage('C++');
+  printLanguage('Python');
+  printLanguage('Javascript');
+})();
 
-  let todo : object = {title: 'TypeScript 공부',done : false};
+(() => {
+  enum Language {
+    JavaScript = 'JS', // 0
+    C = 'C',          // 1
+    Python = 'PY',     // 2
+    Java = 'J'        // 3
+  }
+
+  console.log(Language.Python, Language.C);
+
+  function printLanguage(lang: Language){
+    switch(lang){
+      case Language.JavaScript:
+        console.log('자바스크립트 개발자군요!');
+        break;
+      case Language.Python:
+        console.log('파이썬 개발자군요!');
+        break;
+      case Language.C:
+        console.log('C언어 개발자군요!');
+        break;
+      case Language.Java:
+        console.log('자바 개발자군요!');
+        break;
+    }
+  }
+  // printLanguage(Language.JavaScript);
+  // printLanguage(Language.C);
+  // printLanguage(Language.Java);
+  // printLanguage(Language.Python);
+
+  // printLanguage(Language.JavaScript);
+  // printLanguage(Language.C);
+  // printLanguage(Language.Java);
+  // printLanguage(Language.Python);
 
 })();
+
