9bec9126b89c403816891a22e7f8dcb69131da4b
dltmdrb667 <dltmdrb667@naver.com>
2025-10-28 16:19:23 +0900

    Enum 실습예제

--- workspace/ch06/ex06-06-02.ts
+++ workspace/ch06/ex06-06-02.ts
@@ -2,5 +2,20 @@
 // ex06-06.ts 복사
 
 (()=>{
+  type MessageType = string | number
 
+  // FIXME 타입 에러가 발생하지 않도록 함수의 타입을 지정하세요.
+  function logMessage(msg :MessageType) {
+    console.log(msg);
+  }
+
+  function warnMessage(msg :MessageType) {
+    // 스타일 적용
+    console.log(`%c${msg}`, 'color: yellow; font-size: 20px;');
+  }
+
+  logMessage('hello');
+  logMessage(100);
+  warnMessage('bye');
+  warnMessage(200);
 })();
