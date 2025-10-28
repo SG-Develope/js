8dd8d7e062c516eed01549b8921425c82389312b
dltmdrb667 <dltmdrb667@naver.com>
2025-10-28 16:28:47 +0900

    interface 클래스 타입 지정

--- workspace/ch06/ex06-10.ts
+++ workspace/ch06/ex06-10.ts
@@ -2,37 +2,31 @@
 // ex06-09.ts 복사
 
 (()=>{
-  //타입 별칭 생성
-  // type User = {
-  interface User{
+  // 인터페이스 생성
+  interface User {
     name: string;
     age: number;
   };
 
+  // 인터페이스 사용: 객체의 타입을 지정
+  const haru: User = { name: 'haru', age: 5 };
+  const namu: User = { name: 'namu', age: 8 };
 
-  // 인터페이스 사용 : 객체의 타입을 지정
-  const haru: User = { name: "haru", age: 3 };
-  const namu: User = { name: "namu", age: 2 };
   console.log(haru.age, namu.name.toUpperCase());
 
-  //인터페이스 사용: 함수의 리턴 타입으로 지정
-  function createUser(name: string, age: number) : User{
-    const user = {name, age};
+  // 인터페이스 사용: 함수의 리턴 타입으로 지정
+  function createUser(name: string, age: number): User{
+    const user = { name, age };
     return user;
   }
-  const bom: User = createUser('bom',7);
+  const bom: User = createUser('bom', 11);
   console.log(bom.name, bom.age);
 
-  //인터페이스 사용: 함수의 매개변수의 타입으로 지정
-  function getAge(user: User) {
+  // 인터페이스 사용: 함수의 매개변수의 타입으로 지정
+  function getAge(user: User){
     return user.age;
   }
-
-  console.log(getAge(haru));
-
-
-
-
-
+  console.log(getAge(namu));
+  console.log(getAge(bom));
 
 })();
