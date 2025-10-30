f4e693cd7329b400e9ef0ed638cd3ed33abeac14
dltmdrb667 <dltmdrb667@naver.com>
2025-10-30 10:49:39 +0900

    interface 병합 및 상속

--- workspace/ch06/ex06-16.ts
+++ workspace/ch06/ex06-16.ts
@@ -2,5 +2,20 @@
 // ex06-15.ts 복사
 
 (()=>{
-  
+  interface Todo {
+    id: number;
+    title: string;
+    content: string;
+  }
+
+  interface Todo {
+    done: boolean;
+  }
+
+  const todo: Todo = {
+    id : 1,
+    title: '인터페이스 병합',
+    content: '인터페이스 병합을 해봐요',
+    done: true,
+  }
 })();
