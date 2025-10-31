f17fc9e2562f70549d5598c0b04383e1ba9e48d3
dltmdrb667 <dltmdrb667@naver.com>
2025-10-31 13:45:05 +0900

    객체 복사 (얕은 복사)

--- workspace/ch07/ex07-06.js
+++ workspace/ch07/ex07-06.js
@@ -11,5 +11,17 @@
     const haru2 = haru;
     haru.age++;
     console.log(haru.age, haru2.age); // 6 6
+    const haru3 = Object.assign({}, haru);
+    haru.age++;
+    console.log(haru.age, haru3.age);
+    // 객체 복사 (전개 구문)
+    const haru4 = { ...haru };
+    haru.age++;
+    console.log(haru.age, haru4.age);
+    // 배열 복사
+    const namu2 = { ...namu, bread: '카발리에 킹찰스 스패니얼' };
+    console.log(namu2);
+    const test = Object.fromEntries([['name', '나무'], ['age']]);
+    console.log(test);
 })();
 export {};
