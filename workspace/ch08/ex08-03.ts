81e646c22c79f3bb04a1a27a4d119a372014333e
dltmdrb667 <dltmdrb667@naver.com>
2025-11-03 11:04:58 +0900

    try..catch 문 개념

--- workspace/ch08/ex08-03.ts
+++ workspace/ch08/ex08-03.ts
@@ -1,6 +1,28 @@
 // 에러 핸들링 - try...catch 문으로 에러 처리 1
 // ex08-02.ts 복사
 
-(()=>{
-  
+(() => {
+  console.log("1.f1 실행 완료.");
+
+  function f1() {
+    try {
+      const fn = new Function("return 10;"); //Reference Error
+      console.log("2.f1 실행 완료.",fn());
+    } catch (err) {
+      if (err instanceof Error) {
+        //타입가드
+        console.error(err.message);
+      }
+    } finally {
+      console.log("try..catch끝남");
+    }
+  }
+
+  function f2() {
+    f1();
+    console.log("4.f2 실행 완료.");
+  }
+
+  f2();
+  console.log("5. 프로그램 정상 종료");
 })();
